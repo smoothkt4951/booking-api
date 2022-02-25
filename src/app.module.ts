6b248f8758d1e3fd6144947e5818b401419991cc
khanghoang <khanghoang1702@gmail.com>
2022-02-25 12:33:49 +0700

    commitloi

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,6 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
-import { RoomModule } from './room/room.module';
-
-@Module({
-  imports: [RoomModule],
-=======
 import { AuthModule } from './auth/auth.module';
 
 @Module({
@@ -26,7 +20,6 @@ import { AuthModule } from './auth/auth.module';
       synchronize: true,
     }),
   ],
->>>>>>> 73679577c6ffcb63eeb413ccf6d8c7e1fc95d96b
   controllers: [AppController],
   providers: [AppService],
 })
