4818dcabe5c1021cbc3ce863838416b1f745028e
Sam Smith <h19982312@gmail.com>
2022-02-25 11:09:24 +0700

    fix user_role in request

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,17 +1,13 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { Role } from '../../user/user.entity';
-import { UserService } from '../../user/user.service';
 import { ROLES_KEY } from '../decorators/roles.decorator';
+import { RequestUser } from '../types/request-user.type';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-    constructor(
-        private reflector: Reflector,
-        private readonly userService: UserService,
-    ) {}
+    constructor(private reflector: Reflector) {}
 
     async canActivate(context: ExecutionContext) {
         const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
@@ -21,52 +17,10 @@ export class RolesGuard implements CanActivate {
         if (!requiredRoles) {
             return true;
         }
-        const { user } = context.switchToHttp().getRequest();
-        console.log({ user });
-        const id = user.userId;
-        const userData = await this.userService.findUserBy({ id });
-        return requiredRoles.some((role) => userData.role?.includes(role));
+        const request = context.switchToHttp().getRequest<Request>();
+        const user = request.user as RequestUser;
+        console.log('ROLES GUARD:', user);
+        // return true;
+        return requiredRoles.some((role) => user.userRole?.includes(role));
     }
 }
-
-// @Injectable()
-// export class RolesGuard extends AuthGuard('jwt') {
-//     constructor(
-//         private readonly userService: UserService,
-//         private reflector: Reflector,
-//     ) {
-//         super();
-//     }
-//     async canActivate(context: ExecutionContext): Promise<boolean> {
-//         // call AuthGuard in order to ensure user is injected in request
-//         const baseGuardResult = await super.canActivate(context);
-//         if (!baseGuardResult) {
-//             // unsuccessful authenticƒation return false
-//             return false;
-//         }
-
-//         const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
-//             ROLES_KEY,
-//             [context.getHandler(), context.getClass()],
-//         );
-//         if (!requiredRoles) {
-//             return true;
-//         }
-
-//         // successfull authentication, user is injected
-//         // const { user } = context.switchToHttp().getRequest();
-//         const request = context.switchToHttp().getRequest();
-//         console.log(request);
-//         // console.log('user', user);
-//         // const userId = user.userId;
-//         // const userRole = user.userRole;
-//         // console.log('role', userRole);
-//         // // console.log(id);
-
-//         // const userData = await this.userService.findUserBy({ id: userId });
-
-//         // // console.log(userData);
-
-//         // return requiredRoles.some((role) => userData.role?.includes(role));
-//     }
-// }
