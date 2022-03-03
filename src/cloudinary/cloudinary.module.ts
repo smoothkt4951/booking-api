90210e1f89b6ca2a67195e2f49b1ee5427ecb4ee
khanghoang <khanghoang1702@gmail.com>
2022-03-03 09:20:06 +0700

    doneFixingDependencies

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -8,9 +8,10 @@ import { UserModule } from 'src/user/user.module'
 @Module({
   // imports: [ UserModule],
   providers: [
+    CloudinaryService,
     CloudinaryProvider,
     { provide: 'CLOUD_SERVICE', useClass: CloudinaryService },
   ],
-  exports: [CloudinaryService, CloudinaryProvider],
+  exports: [CloudinaryService, 'Cloudinary'],
 })
 export class CloudinaryModule {}
--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,16 +1,17 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { RoomController } from './room.controller';
-import { RoomService } from './room.service';
-import { MulterModule } from '@nestjs/platform-express';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { RoomEntity } from './entity/room.entity';
-import { RoomsMiddleware } from './middleware/room.middleware';
-import { RequestMethod } from '@nestjs/common/enums/request-method.enum';
-import { UserService } from '../user/user.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
+import { RoomController } from './room.controller'
+import { RoomService } from './room.service'
+import { MulterModule } from '@nestjs/platform-express'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { RoomEntity } from './entity/room.entity'
+import { RoomsMiddleware } from './middleware/room.middleware'
+import { RequestMethod } from '@nestjs/common/enums/request-method.enum'
+import { UserService } from '../user/user.service'
 
-import { UserModule } from 'src/user/user.module';
-import { AuthModule } from 'src/auth/auth.module';
-import { UserEntity } from 'src/user/user.entity';
+import { UserModule } from 'src/user/user.module'
+import { AuthModule } from 'src/auth/auth.module'
+import { UserEntity } from 'src/user/user.entity'
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module'
 @Module({
   imports: [
     MulterModule.register({ dest: './uploads' }),
@@ -18,6 +19,7 @@ import { UserEntity } from 'src/user/user.entity';
     TypeOrmModule.forFeature([UserEntity]),
     UserModule,
     AuthModule,
+    CloudinaryModule,
   ],
   controllers: [RoomController],
   providers: [RoomService, UserService],
@@ -41,6 +43,6 @@ export class RoomModule implements NestModule {
         { path: 'rooms/:roomId', method: RequestMethod.GET },
         { path: 'rooms/:roomId', method: RequestMethod.PUT },
         { path: 'rooms/:roomId', method: RequestMethod.DELETE },
-      );
+      )
   }
 }
